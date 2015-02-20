44f06e535bdea57be56f5a08f3a6a74f5a364353
Brandon Silva <itbrandonsilva@gmail.com>
2015-02-20 01:54:39 +0000

    ASIN entry through form.

--- client/app.js
+++ client/app.js
@@ -1,19 +1,20 @@
 angular.module('app', []).
     controller('AppController', function ($scope) {
-
-        console.log("Controller instantiated.");
-
         var host = window.location.hostname;
         var socket = io(host + ":" + 3042);
-        $scope.sendASIN = function (asin) {
-            socket.emit('asin', { asin: asin });
+
+        $scope.sendASIN = function () {
+            if ( ! $scope.asin ) return;
+            socket.emit('asin', { asin: $scope.asin });
         }
 
         socket.on('connect', function () {
             $scope.sendASIN('HELLO!');
         });
 
-        $scope.reviews = [];
+        $scope.items = [];
+
+        console.log("AppController()");
     })
 ;
 
