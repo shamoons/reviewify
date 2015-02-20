09ead0f4cc82f14d9d3e36ddcb58b2a234d5298a
Shamoon Siddiqui <EmailShamoon@gmail.com>
2015-02-19 20:08:00 -0500

    Added dell

--- parser/testparse.js
+++ parser/testparse.js
@@ -3,17 +3,13 @@ var fs = require('fs');
 var AlchemyAPI = require('alchemy-api');
 var alchemy = new AlchemyAPI('8c11bb2b0bdaaf679cf25b34104914bc8a092c49');
 
-fs.readFile('./data/macbookreview.txt', 'utf-8', function(err, body) {
+fs.readFile('./data/dell1.txt', 'utf-8', function(err, body) {
   if(err) throw err;
 
-  alchemy.category(body, {}, function(err, response) {
+  alchemy.relations(body, {sentiment: 1}, function(err, response) {
     if (err) throw err;
 
-    // See http://www.alchemyapi.com/api/ for format of returned object
-    // var sentiment = response.docSentiment;
-    console.log(response);
-
-    // Do something with data
+    console.log(response.relations[0]);
   });
 });
 
