faa04056c0472ccd97c7e16d8b32a0bce1100d4e
Shamoon Siddiqui <EmailShamoon@gmail.com>
2015-02-19 20:23:07 -0500

    Started to structure function signature

--- parser/testparse.js
+++ parser/testparse.js
@@ -6,10 +6,10 @@ var alchemy = new AlchemyAPI('8c11bb2b0bdaaf679cf25b34104914bc8a092c49');
 fs.readFile('./data/dell1.txt', 'utf-8', function(err, body) {
   if(err) throw err;
 
-  alchemy.relations(body, {sentiment: 1}, function(err, response) {
+  alchemy.keywords(body, {sentiment: 1}, function(err, response) {
     if (err) throw err;
 
-    console.log(response.relations[0]);
+    console.log(response.keywords);
   });
 });
 
