5c2df8d96975f67c8528943ea75b04d5a2a1d71b
Brandon Silva <itbrandonsilva@gmail.com>
2015-02-20 01:10:41 +0000

    Core client files.

---
+++ client/app.js
@@ -0,0 +1,11 @@
+angular.module('app', []).
+    controller('AppController', function ($scope) {
+
+        console.log("Controller instantiated.");
+
+        $scope.sendASIN = function () {
+
+        }
+    })
+;
+
