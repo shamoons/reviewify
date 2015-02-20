f1ca53af84eb774cc9072651fbfa0569803ead56
Shamoon Siddiqui <EmailShamoon@gmail.com>
2015-02-19 21:48:36 -0500

    Added a ton of corpus

--- parser/testparse.js
+++ parser/testparse.js
@@ -1,16 +1,40 @@
-var fs = require('fs');
+var _ = require('lodash');
+
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 
 var AlchemyAPI = require('alchemy-api');
-var alchemy = new AlchemyAPI('8c11bb2b0bdaaf679cf25b34104914bc8a092c49');
+var alchemy = Promise.promisifyAll(new AlchemyAPI('8c11bb2b0bdaaf679cf25b34104914bc8a092c49'));
 
-fs.readFile('./data/macbookreview.txt', 'utf-8', function(err, body) {
-  if(err) throw err;
+var util = require('util');
 
-  alchemy.relations(body, {}, function(err, response) {
-    if (err) throw err;
+var functions = ['sentiment', 'relations', 'concepts', 'keywords'];
 
-    console.log(response.relations);
+var fileData = {}
+fs.readFileAsync('./data/corpus.txt', 'utf-8').then(function(body) {
+  fileData = body;
+  return alchemy.relationsAsync(fileData, {sentiment: 1});
+})
+.then(function(response) {
+  console.log('RELATIONS!');
+  console.log(util.inspect(response.relations, {depth: null}));
+
+  var groupedVerbs = _.groupBy(response.relations, function(relation) {
+    return relation.action.text;
   });
+  console.log('');
+  console.log(util.inspect(groupedVerbs, {depth: null}));
+  return alchemy.sentimentAsync(fileData);
+}).then(function(response) {
+  console.log('\nSENTIMENT!');
+  console.log(util.inspect(response.docSentiment, {depth: null}));
+  return alchemy.keywordsAsync(fileData, {});
+}).then(function(response) {
+  console.log('\nKEYWORDS!');
+  console.log(util.inspect(response.keywords, {depth: null}));
+  return;
+}).catch(function(err) {
+  throw err;
 });
 
 var endResult = [{
