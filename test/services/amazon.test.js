1f7338059de1c66401cd3fb858dd1c9f898bd768
junminstorage <junmin.storage@gmail.com>
2015-02-23 20:00:41 -0500

    add amazon service call test

---
+++ test/services/amazon.test.js
@@ -0,0 +1,18 @@
+var amazon = require('../../services/amazon');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Amazon service', function() {
+	
+  describe('get item review iframe ', function() { 
+	    
+	  it('itemLookup call successfully', function () {
+		  amazon.itemLookup('B00008OE6I', function(err, result){
+			  console.log(result);
+		  });
+		
+	   });
+	  
+  });
+  
+});
