4c8deaef34d28b9c58964a0921254cf9ddd8db79
junminstorage <junmin.storage@gmail.com>
2015-02-26 22:07:16 -0500

    check in alchemy service api client

--- services/alchemy.js
+++ services/alchemy.js
@@ -1,1 +1,43 @@
-//alchemy API call goes here
+//alchemy API call goes here, e.g. analytics api calls
+var _ = require('lodash');
+var util = require('util');
+var Promise = require('bluebird');
+var AlchemyAPI = require('alchemy-api');
+var alchemy = Promise.promisifyAll(new AlchemyAPI('8c11bb2b0bdaaf679cf25b34104914bc8a092c491'));
+var fs = Promise.promisifyAll(require('fs'));
+
+module.exports = {
+
+		'relations' :	function(reviews) {
+			var resultObject = [];
+
+			Promise.map(reviews, function(review) {
+				alchemy.relationsAsync(review.text, {sentiment: 1})
+			}).then(function(reviewRelations) {
+				var verbsWeCareAbout = ['is', 'are', 'has', 'was'];
+				////.....
+			});
+		},
+		
+		
+		'keywords' :	function(reviews) {
+			
+			var transformed = []
+
+			return Promise.map(reviews, function(review) {
+			    return alchemy.keywordsAsync(review, {sentiment: 1});
+			})
+			.each(function(result) {
+			    // This is repeating access for each result
+			    transformed.push(result);
+			})
+			.then(function(results) {
+			    // here 'results' is unmodified results from the api
+			    // array doesn't get updated by 'each' function
+			    // here 'transformed' will contain what you did to each above
+				//console.log(results);
+			    return transformed;
+			});
+			
+		}
+}
